490c5e8053ac288406e9ecc4a664076b7caffadf
Sathananthan Januskar <139611014+SathanJa@users.noreply.github.com>
2024-12-08 07:31:02 +0000

    index php create

---
+++ 04.12.2024_Practical/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>2020/ASP/58</h1>
+    <a href="getTable.php"></a>
+</body>
+</html>
